dd41981fd1e4494f9a4f99533dd32d6b0702349e
Reo Dongmin Lee <reoim10@gmail.com>
2016-12-18 00:35:13 +1100

    Additional views and routing

---
+++ ProductManagement/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductManagement.Models;
+
+namespace ProductManagement.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = this.GetCustomer();   
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = this.GetCustomer().SingleOrDefault(c => c.Id == id);
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomer()
+        {
+            return new List<Customer>
+                       {
+                           new Customer() { Id = 1, Name = "John Smith" },
+                           new Customer() { Id = 2, Name = "Mary Williams" }
+                       };
+        }
+         
+    }
+}
--- ProductManagement/Controllers/MoviesController.cs
+++ ProductManagement/Controllers/MoviesController.cs
@@ -7,33 +7,21 @@ using ProductManagement.Models;
 
 namespace ProductManagement.Controllers
 {
-    using ProductManagement.ViewModels;
+
 
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek" };
-            var customers = new List<Customer>
-                               {
-                                   new Customer() { Name = "Customer 1" },
-                                   new Customer() { Name = "Customer 2" }
-                               };
 
-            var viewModel = new RandomMovieViewModel { Movie = movie, Customers = customers };
-            return View(viewModel);
+        public ViewResult Index()
+        {
+            var moives = this.GetMovies();
+            return View(moives);
         }
 
-        public ActionResult Edit(int id)
+        private IEnumerable<Movie> GetMovies()
         {
-            return Content("id=" + id);
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return new List<Movie> { new Movie() { Id = 1, Name = "Shrek" }, new Movie() { Id = 2, Name = "Wall-e" } };
         }
     }
 }
