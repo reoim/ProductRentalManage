1e3de90b8f2e51085254709a55f2b3dfd29bb2f5
Reo Dongmin Lee <reoim10@gmail.com>
2016-12-18 15:38:29 +1100

    update database

--- ProductManagement/Controllers/CustomersController.cs
+++ ProductManagement/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,27 +10,32 @@ namespace ProductManagement.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            this._context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this._context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = this.GetCustomer();   
+            var customers = this._context.Customers.Include(c => c.MembershipType).ToList();   
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = this.GetCustomer().SingleOrDefault(c => c.Id == id);
+            var customer = this._context.Customers.SingleOrDefault(c => c.Id == id);
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomer()
-        {
-            return new List<Customer>
-                       {
-                           new Customer() { Id = 1, Name = "John Smith" },
-                           new Customer() { Id = 2, Name = "Mary Williams" }
-                       };
-        }
-         
+     
     }
 }
--- ProductManagement/Models/Customer.cs
+++ ProductManagement/Models/Customer.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductManagement.Models
 {
+    
+
     public class Customer
     {
         public int Id { get; set; }
-
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
     }
 }
